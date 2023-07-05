e92e59ecc9d774964357506978359f3d957e57da
dastalein <dastalein@gmail.com>
2023-07-05 14:49:50 +0300

    Add one migrate

---
+++ RockyShop/Migrations/20230705083610_One_connect_Db.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RockyShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class OneconnectDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
