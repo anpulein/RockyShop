cd9c00f975708ee39c49ff438566cabe42ec6a5f
dastalein <dastalein@gmail.com>
2023-07-14 09:04:20 +0300

    add view category

--- RockyShop/Controllers/CategoryController.cs
+++ RockyShop/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RockyShop.Data;
+using RockyShop.Models;
 
 namespace RockyShop.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
     // GET
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Category> objList = _db.Category;
+        return View(objList);
     }
 }
