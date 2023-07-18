eb4da225eb011952b0f1e5a0ba59ff79c51f2d86
dastalein <dastalein@gmail.com>
2023-07-18 15:12:10 +0300

    Интерфейс создания и обновления товара Product model

--- RockyShop/Controllers/ProductController.cs
+++ RockyShop/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RockyShop.Data;
 using RockyShop.Models;
 
@@ -27,56 +28,42 @@ public class ProductController : Controller
     }
 
 
-    // GET - CREATE
-    public IActionResult Create()
+    // GET - UPSERT
+    public IActionResult UpSert(int? id)
     {
-        return View();
+        IEnumerable<SelectListItem> CateroryDropDown = _db.Category.Select(s => new SelectListItem 
+        {
+            Text = s.Name,
+            Value = s.Id.ToString()
+        });
+        
+        ViewBag.CateroryDropDown = CateroryDropDown;
+        
+        Product product = new Product();
+
+        if (id == null)
+        {
+            // this is for create
+            return View(product);
+        }
+        else
+        {
+            product = _db.Product.Find(id);
+            if (product == null) return NotFound();
+            
+            return View(product);
+        }
     }
 
-    // POST - CREATE
+    // POST - UPSERT
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Create(Category obj)
+    public IActionResult UpSert(Category obj)
     {
         // Валидация на стороне сервера
         if (ModelState.IsValid)
         {
             _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        return View(obj);
-    }
-
-    // GET - EDIT
-    public IActionResult Edit(int? id)
-    {
-        if (id == null || id == 0)
-        {
-            return NotFound();
-        }
-        
-        var obj = _db.Category.Find(id);
-
-        if (obj == null)
-        {
-            return NotFound();
-            
-        }
-        
-        return View(obj);
-    }
-    
-    // POST - Edit
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult Edit(Category obj)
-    {
-        // Валидация на стороне сервера
-        if (ModelState.IsValid)
-        {
-            _db.Category.Update(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
